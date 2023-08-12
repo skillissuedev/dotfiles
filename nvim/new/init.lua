8cb3258126cb334d9d6113c57e8544aa2a2689df
unknownbruh <kuzasteam@inbox.ru>
2023-08-12 23:00:31 +0700

    config update(alacritty, new theme in nvim)

---
+++ nvim/new/init.lua
@@ -0,0 +1,11 @@
+require("plugins")
+require("keybindings")
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.g.autoindent = true
+vim.g.smarttab = true
+vim.g.tabstop = 2
+vim.g.shiftwidth = 4
+vim.g.softtabstop = 2
+vim.g.mouse = true
---
+++ nvim/new/lua/keybindings.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<C-e>', ':NERDTreeToggle<CR>', {silent = true })
+
